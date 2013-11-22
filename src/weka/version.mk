41a2fb37c576004815dd04db355c7b7c9a9cdb3c
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 16:05:07 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/weka/version.mk
+++ src/weka/version.mk
@@ -1,3 +1,15 @@
-NAME    = weka
-VERSION = 3.7.7
-RELEASE = 0
+NAME               = weka
+VERSION            = 3.7.7
+RELEASE            = 0
+PKGROOT            = /opt/weka
+
+SRC_SUBDIR         = weka
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
