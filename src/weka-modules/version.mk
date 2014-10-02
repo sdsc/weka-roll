c6bb2e0d2720870669a492b890cfce4cb4e53388
Jim Hayes <jhayes@sdsc.edu>
2014-10-01 19:24:30 -0700

    Use canonical modulefile/roll-test build files.

--- src/weka-modules/version.mk
+++ src/weka-modules/version.mk
@@ -1,8 +1,11 @@
-NAME        = weka-modules
-RELEASE     = 1
-PKGROOT     = /opt/modulefiles/applications/weka
+PACKAGE     = weka
+CATEGORY    = applications
 
-VERSION_SRC = $(REDHAT.ROOT)/src/weka/version.mk
+NAME        = $(PACKAGE)-modules
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
