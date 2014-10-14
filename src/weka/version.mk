3e959ce81412a30d7a4fa00ec9d0acf1c328f172
Jim Hayes <jhayes@sdsc.edu>
2014-10-14 13:11:14 -0700

    Upgrade version.

--- src/weka/version.mk
+++ src/weka/version.mk
@@ -1,16 +1,16 @@
 NAME           = weka
-VERSION        = 3.7.10
+VERSION        = 3.7.11
 RELEASE        = 0
 PKGROOT        = /opt/weka
 
 SRC_SUBDIR     = weka
 
 SOURCE_NAME    = $(NAME)
-SOURCE_SUFFIX  = tgz
-SOURCE_VERSION = $(VERSION)
+SOURCE_SUFFIX  = zip
+SOURCE_VERSION = 3-7-11
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TGZ_PKGS       = $(SOURCE_PKG)
+ZIP_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
