e0b7f4177addfe998616f85bf68a418a0e9ae636
Jim Hayes <jhayes@sdsc.edu>
2014-12-30 09:52:29 -0800

    Upgrade package version.

--- src/weka/version.mk
+++ src/weka/version.mk
@@ -1,13 +1,13 @@
 NAME           = sdsc-weka
-VERSION        = 3.7.11
-RELEASE        = 1
+VERSION        = 3.7.12
+RELEASE        = 0
 PKGROOT        = /opt/weka
 
 SRC_SUBDIR     = weka
 
 SOURCE_NAME    = weka
 SOURCE_SUFFIX  = zip
-SOURCE_VERSION = 3-7-11
+SOURCE_VERSION = 3-7-12
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
